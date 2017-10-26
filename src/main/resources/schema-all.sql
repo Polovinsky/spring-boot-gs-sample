c915b092ef90199eb97e11567dba8e25ff86e444
Mateus Oliveira Malaquias <mateus.malaquias@fcamara.com.br>
2017-10-26 13:56:25 -0200

    Fixed create database for dev/test when the application was started

--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -2,12 +2,10 @@ DROP TABLE user IF EXISTS;
 
 CREATE TABLE user (
     id BIGINT IDENTITY NOT NULL PRIMARY KEY,
-    firstName VARCHAR(20),
-    lastName VARCHAR(20),
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
     email VARCHAR(50),
     password VARCHAR(50)
 );
 
---insert into user (id, firstName, lastName, email, password) values (1, "Luke", "Skywalker", "luke.sky@jedi.com", "jedirocks");
---insert into user (id, firstName, lastName, email, password) values (2, "Pocahontas", "Disney", "poca@disney.net", "hontas");
---insert into user (id, firstName, lastName, email, password) values (3, "Gandalf", "Mithrandir", "gandalf@tolkien.com", "oBranco");
+INSERT INTO user (id, first_name, last_name, email, password) VALUES (1, 'Ramesh', 'Ahmedabad', 'ramesh@gmail.com', '2000');
